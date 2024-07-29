b95d1782639b135105985eebaf424aeda4b29349
2021itb <syw7777@naver.com>
2024-07-29 21:49:22 +0900

    feat: chat_screen.dart TextField 추가, colorStyles 색상 추가, 아이콘 이미지 추가

--- lib/presentation/screen/chat/chat_screen.dart
+++ lib/presentation/screen/chat/chat_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:with_u/ui/text_styles.dart';
 
+import '../../../ui/color_styles.dart';
+
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
 
@@ -16,6 +18,53 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text(
           'With U',
           style: TextStyles.headingH4,
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Row(
+          children: [
+            IconButton(
+              icon: Image.asset('assets/more_options.png'),
+              onPressed: () {
+                // 플러스 버튼 클릭 시 실행될 함수
+              },
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  suffixIcon: GestureDetector(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 6),
+                      child: Image.asset('assets/send.png'),
+                    ),
+                    onTap: () {},
+                  ),
+                  filled: true,
+                  fillColor: ColorStyles.neutralLight,
+                  border: InputBorder.none,
+                  hintText: ' 위드유에게 이야기해 보세요',
+                  hintStyle: TextStyle(color: Colors.grey),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(71),
+                    ),
+                    borderSide: BorderSide(
+                      color: ColorStyles.neutralLight,
+                    ), // 기본 아웃라인 색상
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(71),
+                    ),
+                    borderSide: BorderSide(
+                      color: ColorStyles.neutralLight,
+                    ), // 기본 아웃라인 색상
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/ui/color_styles.dart
+++ lib/ui/color_styles.dart
@@ -5,5 +5,7 @@ abstract class ColorStyles {
   static const Color lightest = Color(0xFFEAF2FF);
   static const Color white = Color(0xFFFFFFFF);
 
+  static const Color neutralLight = Color(0xFFF8F9FE);
+
   static const Color darkLightest = Color(0xFF8F9098);
 }
