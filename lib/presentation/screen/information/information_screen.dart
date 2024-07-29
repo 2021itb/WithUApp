1f40091864dc0010ec1feed6ad990f62ef33c846
2021itb <syw7777@naver.com>
2024-07-29 17:08:11 +0900

    feat: information 폴더 및 screen, view_model 파일 추가

---
+++ lib/presentation/screen/information/information_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class InformationScreen extends StatefulWidget {
+  const InformationScreen({super.key});
+
+  @override
+  State<InformationScreen> createState() => _InformationScreenState();
+}
+
+class _InformationScreenState extends State<InformationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
