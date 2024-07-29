7a6dbec7e1ac5ec3494ab6c4f0c8090a736a17d4
Kimdahee <dh980502@gmail.com>
2024-07-29 15:15:13 +0900

    style : text styles, color styles 정의

---
+++ lib/ui/color_styles.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+abstract class ColorStyles {
+  static const Color primary100 = Color(0xFFA8CAFE);
+  static const Color lightest = Color(0xFFEAF2FF);
+  static const Color white = Color(0xFFFFFFFF);
+}
