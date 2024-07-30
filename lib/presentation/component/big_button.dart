873a0e9c3f7ddf2dbc87b97fc4b2c6a0fb017cd4
Hunhee Lee <hunhee.dev@gmail.com>
2024-07-30 12:27:27 +0900

    [fix] BigButton UI 수정 - 내부 Padding 제거

--- lib/presentation/component/big_button.dart
+++ lib/presentation/component/big_button.dart
@@ -18,32 +18,25 @@ class BigButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.only(
-          left: 30,
-          right: 30,
-          top: 10,
-          bottom: 10,
-        ),
-        child: GestureDetector(
-          onTap: () {
-            context.go(path);
-          },
-          child: Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(8)),
-                color: backgroundColor),
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  text,
-                  style: TextStyles.actionM.copyWith(color: textColor),
-                ),
-              ),
+    return GestureDetector(
+      onTap: () {
+        context.go(path);
+      },
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(8)),
+            color: backgroundColor),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              text,
+              style: TextStyles.actionM.copyWith(color: textColor),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
